11ef7415c0c4e558cded078a3765a219a62f23f6
Rathees-Koneswarann <ratheesreo@gmail.com>
2021-12-15 16:35:34 +0530

    removed contact modal

--- contactform.js
+++
@@ -1,8 +0,0 @@
-function clearInputPlace() {
-  function test() {
-  document.getElementById("name").value='';
-  document.getElementById("email").value='';
-  document.getElementById("message").value='';
-}
-return test();
-}
